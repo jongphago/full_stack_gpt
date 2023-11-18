770494e9dd901eb21cc412f9a4d2298b5c789543
jongphago <jonghyun.d.kim@gmail.com>
2023-11-19 08:20:58 +0900

    feat(Model.ipynb): ChatOpenAI 모델과 PromptTemplate을 사용하는 코드 추가 피트(Model.ipynb): ChatOpenAI 모델과 PromptTemplate을 사용하여 채팅 기능을 구현하는 코드를 추가했습니다. ChatOpenAI 모델은 온라인으로 스트리밍되는 채팅을 처리하고, PromptTemplate은 템플릿 기반의 프롬프트를 생성하는 데 사용됩니다. 이를 통해 사용자가 입력한 질문에 대한 답변을 생성하는 채팅 기능을 구현할 수 있습니다.

---
+++ Model.py
@@ -0,0 +1,58 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.few_shot import FewShotPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
+from langchain.prompts import PromptTemplate
+
+
+chat = ChatOpenAI(
+    temperature=0.1,
+    streaming=True,
+    callbacks=[
+        StreamingStdOutCallbackHandler(),
+    ],
+)
+
+examples = [
+    {
+        "question": "What do you know about France?",
+        "answer": """
+    Here is what I know:
+    Capital: Paris
+    Language: French
+    Food: Wine and Cheese
+    Currency: Euro
+    """,
+    },
+    {
+        "question": "What do you know about Italy?",
+        "answer": """
+    I know this:
+    Capital: Rome
+    Language: Italian
+    Food: Pizza and Pasta
+    Currency: Euro
+    """,
+    },
+    {
+        "question": "What do you know about Greece?",
+        "answer": """
+    I know this:
+    Capital: Athens
+    Language: Greek
+    Food: Souvlaki and Feta Cheese
+    Currency: Euro
+    """,
+    },
+]
+
+example_prompt = PromptTemplate.from_template("Human: {question}\nAI: {answer}")
+prompt = FewShotPromptTemplate(
+    example_prompt=example_prompt,
+    examples=examples,
+    suffix="Human: What do you know about {country}?",
+    input_variables=["country"],
+)
+
+chain = prompt | chat
+
+chain.invoke({"country": "Korea"})
