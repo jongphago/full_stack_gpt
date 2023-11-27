82f64a220a8ad5a82ebcf8909fb92093875dd4e5
jongphago <jonghyun.d.kim@gmail.com>
2023-11-27 20:25:34 +0900

    Initialize 4.4 Serialization and Composition

--- Model.py
+++ Model.py
@@ -1,24 +1,7 @@
-from typing import Any, Dict, List
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts.few_shot import FewShotPromptTemplate
 from langchain.callbacks import StreamingStdOutCallbackHandler
-from langchain.prompts import PromptTemplate
-from langchain.prompts.example_selector import LengthBasedExampleSelector
-from langchain.prompts.example_selector.base import BaseExampleSelector
 
-
-class RandomExampleSelector(BaseExampleSelector):
-    def __init__(self, examples):
-        self.examples = examples
-
-    def add_example(self, example):
-        self.examples.append(example)
-
-    def select_examples(self, input_variables):
-        from random import choice
-
-        return [choice(self.examples)]
-
+prompt = None
 
 chat = ChatOpenAI(
     temperature=0.1,
@@ -28,58 +11,6 @@ chat = ChatOpenAI(
     ],
 )
 
-examples = [
-    {
-        "question": "What do you know about France?",
-        "answer": """
-    Here is what I know:
-    Capital: Paris
-    Language: French
-    Food: Wine and Cheese
-    Currency: Euro
-    """,
-    },
-    {
-        "question": "What do you know about Italy?",
-        "answer": """
-    I know this:
-    Capital: Rome
-    Language: Italian
-    Food: Pizza and Pasta
-    Currency: Euro
-    """,
-    },
-    {
-        "question": "What do you know about Greece?",
-        "answer": """
-    I know this:
-    Capital: Athens
-    Language: Greek
-    Food: Souvlaki and Feta Cheese
-    Currency: Euro
-    """,
-    },
-]
-
-example_prompt = PromptTemplate.from_template("Human: {question}\nAI: {answer}")
-example_selector = LengthBasedExampleSelector(
-    examples=examples,
-    example_prompt=example_prompt,
-    max_length=80,
-)
-example_selector = RandomExampleSelector(
-    examples=examples,
-)
-prompt = FewShotPromptTemplate(
-    example_prompt=example_prompt,
-    example_selector=example_selector,
-    suffix="Human: What do you know about {country}?",
-    input_variables=["country"],
-)
 
 format = prompt.format(country="Korean")
 print(format)
-
-chain = prompt | chat
-
-chain.invoke({"country": "Korea"})
