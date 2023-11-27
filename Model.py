77a23fdf014ff38501b78ae8b7a8024d4747f90d
jongphago <jonghyun.d.kim@gmail.com>
2023-11-27 21:10:20 +0900

    feat(Model.py): 4.4 프롬프트 로드 기능 추가 feat: prompt.json 및 prompt.yaml 파일 추가 Model.py에서 prompt 변수는 이제 load_prompt 함수를 사용하여 "./prompt.json"에서 프롬프트를 로드합니다. 이 변경으로 인해 프롬프트를 더 쉽게 관리하고 수정할 수 있습니다.
    새로운 prompt.json 및 prompt.yaml 파일은 프롬프트 템플릿을 저장하고 있습니다. 이 파일들은 프롬프트를 중앙에서 관리하고, 프롬프트의 변경 사항을 추적하며, 프롬프트를 더 쉽게 수정하고 테스트할 수 있게 합니다.

--- Model.py
+++ Model.py
@@ -1,7 +1,8 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import StreamingStdOutCallbackHandler
+from langchain.prompts import load_prompt
 
-prompt = None
+prompt = load_prompt("./prompt.json")
 
 chat = ChatOpenAI(
     temperature=0.1,
