a9618c592c6f3ea6688a90ba1bff881a4bb0f018
jongphago <jonghyun.d.kim@gmail.com>
2023-11-27 21:25:38 +0900

    feat(Model.py): 4.4 프롬프트 템플릿 및 파이프라인 추가 프롬프트 로딩 방식이 변경되어, 이제는 PromptTemplate 클래스를 사용하여 프롬프트를 생성합니다. 또한, 새로운 PipelinePromptTemplate 클래스를 사용하여 프롬프트의 파이프라인을 생성합니다. 이러한 변경으로 인해 프롬프트의 생성과 사용이 더욱 간편해졌습니다. 또한, 이 변경으로 인해 프롬프트의 유연성과 재사용성이 향상되었습니다. 이제 각각의 프롬프트 템플릿을 개별적으로 테스트하고 수정할 수 있으며, 필요에 따라 쉽게 프롬프트 파이프라인을 조정할 수 있습니다.

--- Model.py
+++ Model.py
@@ -1,8 +1,9 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import StreamingStdOutCallbackHandler
-from langchain.prompts import load_prompt
+from langchain.prompts import PromptTemplate
+from langchain.prompts.pipeline import PipelinePromptTemplate
+from numpy import character
 
-prompt = load_prompt("./prompt.json")
 
 chat = ChatOpenAI(
     temperature=0.1,
@@ -12,6 +13,58 @@ chat = ChatOpenAI(
     ],
 )
 
+intro = PromptTemplate.from_template(
+    """Yor are a role playing assistant.
+    And you are impersonating a {character}
+    """
+)
 
-format = prompt.format(country="Korean")
+example = PromptTemplate.from_template(
+    """This is an example of how you talk:
+    
+    Human: {example_question}
+    You: {example_answer}
+    """
+)
+
+start = PromptTemplate.from_template(
+    """Start now!
+    
+    Human: {question}
+    You:
+    """
+)
+
+final = PromptTemplate.from_template(
+    """{intro}
+
+    {example}
+    
+    {start}
+    """
+)
+
+prompts = [
+    ("intro", intro),
+    ("example", example),
+    ("start", start),
+]
+full_prompt = PipelinePromptTemplate(final_prompt=final, pipeline_prompts=prompts)
+
+format = full_prompt.format(
+    character="Pirate",
+    example_question="What is your location?",
+    example_answer="Arrrg! That is a secret!! arg arg!!",
+    question="What is your favoriate food?",
+)
 print(format)
+
+chain = full_prompt | chat
+chain.invoke(
+    {
+        "character": "Pirate",
+        "example_question": "What is your location?",
+        "example_answer": "Arrrg! That is a secret!! arg arg!!",
+        "question": "What is your favoriate food?",
+    }
+)
