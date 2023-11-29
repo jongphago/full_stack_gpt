607631bef47998e687b0a56fe716f766e7cd8978
jongphago <jonghyun.d.kim@gmail.com>
2023-11-29 21:01:03 +0900

    refactor(Model.py): 캐시 시스템 InMemoryCache, SQLiteCache 추가 fix(.gitignore): *.db 파일 무시 설정 추가

--- Model.py
+++ Model.py
@@ -1,9 +1,12 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import StreamingStdOutCallbackHandler
-from langchain.prompts import PromptTemplate
-from langchain.prompts.pipeline import PipelinePromptTemplate
-from numpy import character
+from langchain.globals import set_llm_cache, set_debug
+from langchain.cache import InMemoryCache, SQLiteCache
+from time import time
 
+# set_llm_cache(InMemoryCache())
+set_llm_cache(SQLiteCache("cache.db"))
+set_debug(True)
 
 chat = ChatOpenAI(
     temperature=0.1,
@@ -13,58 +16,13 @@ chat = ChatOpenAI(
     ],
 )
 
-intro = PromptTemplate.from_template(
-    """Yor are a role playing assistant.
-    And you are impersonating a {character}
-    """
-)
+begin = time()
+chat.predict("How do you make italian pasta")
+end1 = time() - begin
 
-example = PromptTemplate.from_template(
-    """This is an example of how you talk:
-    
-    Human: {example_question}
-    You: {example_answer}
-    """
-)
+begin = time()
+chat.predict("How do you make italian pasta")
+end2 = time() - begin
 
-start = PromptTemplate.from_template(
-    """Start now!
-    
-    Human: {question}
-    You:
-    """
-)
-
-final = PromptTemplate.from_template(
-    """{intro}
-
-    {example}
-    
-    {start}
-    """
-)
-
-prompts = [
-    ("intro", intro),
-    ("example", example),
-    ("start", start),
-]
-full_prompt = PipelinePromptTemplate(final_prompt=final, pipeline_prompts=prompts)
-
-format = full_prompt.format(
-    character="Pirate",
-    example_question="What is your location?",
-    example_answer="Arrrg! That is a secret!! arg arg!!",
-    question="What is your favoriate food?",
-)
-print(format)
-
-chain = full_prompt | chat
-chain.invoke(
-    {
-        "character": "Pirate",
-        "example_question": "What is your location?",
-        "example_answer": "Arrrg! That is a secret!! arg arg!!",
-        "question": "What is your favoriate food?",
-    }
-)
+print(end1)  # 12.528858184814453
+print(end2)  # 0.010240554809570312
