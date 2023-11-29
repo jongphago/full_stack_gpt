667c7f56144b6684223cf7c4164f87b3a2064866
jongphago <jonghyun.d.kim@gmail.com>
2023-11-29 21:36:39 +0900

    feat (Model.py): get_openai_callback를 사용 사용량 측정

--- Model.py
+++ Model.py
@@ -1,28 +1,10 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.callbacks import StreamingStdOutCallbackHandler
-from langchain.globals import set_llm_cache, set_debug
-from langchain.cache import InMemoryCache, SQLiteCache
-from time import time
-
-# set_llm_cache(InMemoryCache())
-set_llm_cache(SQLiteCache("cache.db"))
-set_debug(True)
+from langchain.callbacks import get_openai_callback
 
 chat = ChatOpenAI(
     temperature=0.1,
-    streaming=True,
-    callbacks=[
-        StreamingStdOutCallbackHandler(),
-    ],
 )
 
-begin = time()
-chat.predict("How do you make italian pasta")
-end1 = time() - begin
-
-begin = time()
-chat.predict("How do you make italian pasta")
-end2 = time() - begin
-
-print(end1)  # 12.528858184814453
-print(end2)  # 0.010240554809570312
+with get_openai_callback() as usage:
+    chat.predict("What is the recipe for soju")
+    print(usage)
