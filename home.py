2a16d35f30ae2752e60dd53a66e4627d53ab4170
jongphago <jonghyun.d.kim@gmail.com>
2023-12-03 18:27:19 +0900

    feat(home.py): (7.0) Streamlit 기반의 새로운 홈페이지 추가 Streamlit을 사용하여 새로운 홈페이지를 생성하였습니다. 이 페이지는 "Hello World"라는 제목과 하위 헤더, 그리고 마크다운 형식의 코드 블록을 포함하고 있습니다. 이 변경은 사용자에게 더 나은 첫 인상을 제공하고, 코드 예제를 통해 사이트의 기능을 더 잘 이해할 수 있도록 돕기 위함입니다.

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Hello World")
+
+st.subheader("This is a subheader")
+
+st.markdown(
+    """### This is a markdown
+    ```python
+    print('hello, world!')
+    ```
+    """
+)
