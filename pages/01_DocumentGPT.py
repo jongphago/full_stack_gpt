76b687316a33fe82bc386d672e81c9d431d147b6
jongphago <jonghyun.d.kim@gmail.com>
2023-12-03 20:50:57 +0900

    feat(01_DocumentGPT.py): 채팅 메시지 세션 상태 관리 기능 추가 이전에는 채팅 메시지가 페이지를 새로 고칠 때마다 사라졌습니다. 이제 채팅 메시지는 세션 상태에서 관리되므로 페이지를 새로 고칠 때마다 유지됩니다. 이를 통해 사용자는 이전 대화를 계속 볼 수 있습니다. 또한, AI는 사용자의 메시지에 대한 응답을 생성하고 이를 채팅에 표시합니다. 이러한 변경으로 인해 사용자와 AI 간의 대화가 더 자연스럽게 이루어질 수 있습니다.

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -8,19 +8,31 @@ st.set_page_config(
 
 st.title("DocumentGPT")
 
-with st.chat_message("human"):
-    st.write("Hello, I am a human!")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
 
-with st.chat_message("ai"):
-    st.write("Hello, I am an AI!")
 
-st.chat_input("Say something to me!")
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.write(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
 
-with st.status("Embedding file...", expanded=True) as status:
-    time.sleep(3)
-    st.write("Getting the file...")
-    time.sleep(3)
-    st.write("Embeddingthe file...")
-    time.sleep(3)
-    st.write("Caching the file...")
-    status.update(label="Error", state="error")
+
+for message in st.session_state["messages"]:
+    send_message(
+        message["message"],
+        message["role"],
+        save=False,
+    )
+
+
+message = st.chat_input("Say something to me!")
+
+if message:
+    send_message(message, "human")
+    time.sleep(2)
+    send_message(f"You said: {message}", "ai")
+    
+    with st.sidebar:
+        st.write(st.session_state["messages"])
