5be878893fa1e1d68cbbbb5f9f114d3ff675cddc
jongphago <jonghyun.d.kim@gmail.com>
2023-12-03 19:38:22 +0900

    feat(home.py): (7.3) 스트림릿 대시보드 업데이트 스트림릿 대시보드의 기본 레이아웃이 변경되었습니다. 이제 현재 시간을 제목으로 표시하고, 사이드바에 입력 필드를 추가하였습니다. 또한, 새로운 탭 구조를 도입하여 사용자가 쉽게 내용을 탐색할 수 있도록 하였습니다. 이러한 변경은 사용자 경험을 향상시키기 위해 이루어졌습니다.

--- home.py
+++ home.py
@@ -1,13 +1,21 @@
 import streamlit as st
+from datetime import datetime
 
-st.title("Hello World")
+today = datetime.today().strftime("%H:%M:%S")
 
-st.subheader("This is a subheader")
+st.title(today)
 
-st.markdown(
-    """### This is a markdown
-    ```python
-    print('hello, world!')
-    ```
-    """
-)
+with st.sidebar:
+    st.title("sidebar title")
+    st.text_input("xxx")
+    
+tab1, tab2, tab3 = st.tabs(["tab1", "tab2", "tab3"])
+
+with tab1:
+    st.write("tab1")
+    
+with tab2:
+    st.write("tab2")
+    
+with tab3: 
+    st.write("tab3")
