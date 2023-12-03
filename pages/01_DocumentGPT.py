9d31a827c68b864a27c1c01ed195197cd8c7e87a
jongphago <jonghyun.d.kim@gmail.com>
2023-12-03 20:22:19 +0900

    feat(01_DocumentGPT.py): 채팅 인터페이스 및 파일 임베딩 상태 업데이트 추가 01_DocumentGPT.py 페이지에 채팅 인터페이스를 추가하여 사용자와 AI 간의 대화를 가능하게 했습니다. 또한, 파일 임베딩 과정의 상태를 실시간으로 업데이트하는 기능을 추가하여 사용자가 프로세스를 더 잘 이해할 수 있도록 했습니다. 이러한 변경은 사용자 경험을 향상시키고, 사용자가 AI와의 상호작용을 더욱 즐길 수 있게 합니다.

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -7,3 +7,20 @@ st.set_page_config(
 )
 
 st.title("DocumentGPT")
+
+with st.chat_message("human"):
+    st.write("Hello, I am a human!")
+
+with st.chat_message("ai"):
+    st.write("Hello, I am an AI!")
+
+st.chat_input("Say something to me!")
+
+with st.status("Embedding file...", expanded=True) as status:
+    time.sleep(3)
+    st.write("Getting the file...")
+    time.sleep(3)
+    st.write("Embeddingthe file...")
+    time.sleep(3)
+    st.write("Caching the file...")
+    status.update(label="Error", state="error")
